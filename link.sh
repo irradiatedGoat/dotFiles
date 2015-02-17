2f7c32ba46fb19e92ba6b06b86af84474f69f076
John Doe <J@d.oe>
2015-02-17 12:57:49 -0800

    Fixed script a touch more

--- link.sh
+++ link.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-
+mv ~/.vimrc dotFiles/vimrc.bak
+mv ~/.tmux.conf dotFiles/tmux.conf.bak 
+mv ~/.vim dotFiles/vim.bak
 ln -s dotFiles/vim ~/.vim 
 ln -s dotFiles/tmux.conf ~/.tmux.conf 
-ln -s vimrc ~/.vimrc 
+ln -s dotFiles/vimrc ~/.vimrc 
 mkdir ~/.undofiles
